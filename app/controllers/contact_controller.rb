1f09eb79cf7698532e0170f4f7e1fada355a8ce9
jasjeet <jasjeetgill93@gmail.com>
2017-07-21 11:20:31 -0400

    from jas

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,8 @@
+class ContactController < ApplicationController
+
+  def index
+    
+    end
+   def show
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+ 
 
   # resources :checkout
   # resources :home
   # resources :products
   # resources :product_detail
   # resources :auth
-
+  resources  :contact
+  resources :checkout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   
+
 end
