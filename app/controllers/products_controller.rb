eacfedf37ad249168951fb85b7c8b3a769996fe2
kimihudu <kimihudu@gmail.com>
2017-07-24 21:31:19 -0400

    update home/product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @brands = Brand.all
+    @categories = Category.all
+    @products = Product.all
   end
 
   def show
+    @brands = Brand.all
+    @categories = Category.all
+    @products = Product.all
   end
 end
